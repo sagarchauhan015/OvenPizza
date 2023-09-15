36aa1d70fbc7f0c12a0702681aa79a5aa64d846d
Sagar Chauhan <sagarchauhan015@gmail.com>
2023-09-15 21:57:34 +0530

    created dropbox for items

--- index.js
+++ index.js
@@ -2,31 +2,27 @@ function onDragStart(event) {
     event
       .dataTransfer
       .setData('text/plain', event.target.id);
-
-    // event.currentTarget.style.width = '300px';
-    // event.currentTarget.style.height = '300px';
-
-
 }
+
 
 function onDragOver(event) {
     event.preventDefault();
 }
 
+let Index = 10;
 function onDrop(event) {
-    const id = event
-      .dataTransfer
-      .getData('text');
+    const id = event.dataTransfer.getData('text');
 
     const draggableElement = document.getElementById(id);
     draggableElement.style.width = '300px';
     draggableElement.style.height = '300px';
+    draggableElement.style.position = 'absolute';
+    draggableElement.style.zIndex = Index;
 
     const dropzone = event.target;
     dropzone.appendChild(draggableElement);
 
-    event
-    .dataTransfer
-    .clearData();
+    Index++;
+    event.dataTransfer.clearData();
 
 }
